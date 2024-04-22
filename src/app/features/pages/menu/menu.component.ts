6132b37b20dbcf1e4faa6460d77c438cc82b50d0
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-04-22 18:15:43 -0300

    feat: add menu page

---
+++ src/app/features/pages/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Página inicial',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/home',
+        style: {'margin-left': 'auto'}
+      },
+      {
+        label: 'Modos de armazenamento',
+        routerLink: '/armazenamento',
+        icon: 'pi pi-fw pi-box',
+      },
+      {
+        label: 'Sobre nós',
+        routerLink: '/sobre-nos',
+        icon: 'pi pi-fw pi-user',
+      },
+    ];
+  }
+}
