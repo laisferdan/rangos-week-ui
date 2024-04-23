d6b6b082636f78953ee0b9d299d9083171c2e3d7
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-04-22 22:47:43 -0300

    feat: add Capitalize First Letter pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimengUiControlsModule } from '../ui-controls/primeng-ui-controls/primeng-ui-controls.module';
 import { MenuComponent } from '../features/pages/menu/menu.component';
-
-
+import { HomeComponent } from '../features/pages/home/home.component';
+import { CapitalizeFirstLetterPipe } from './pipes/capitalize-first-letter.pipe';
 
 @NgModule({
-  declarations: [MenuComponent],
-  imports: [
+  declarations: [MenuComponent, HomeComponent],
+  exports: [
     CommonModule,
     PrimengUiControlsModule,
+    MenuComponent,
+    HomeComponent,
   ],
-  exports: [
-    PrimengUiControlsModule,
-    MenuComponent
-  ]
+  imports: [CommonModule, PrimengUiControlsModule, CapitalizeFirstLetterPipe],
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/app/ui-controls/primeng-ui-controls/primeng-ui-controls.module.ts
+++ src/app/ui-controls/primeng-ui-controls/primeng-ui-controls.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
-
+import { SplitterModule  } from 'primeng/splitter';
+import {ButtonModule} from 'primeng/button';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    MenubarModule 
+    MenubarModule,
+    SplitterModule ,
+    ButtonModule
   ],
   exports: [
-    MenubarModule 
+    MenubarModule,
+    SplitterModule ,
+    ButtonModule
   ] 
 })
 export class PrimengUiControlsModule { }
