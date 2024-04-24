16ce5ea93ba2fa525ac601671eb53249f8131bfa
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-04-23 21:03:35 -0300

    feat: add new primeng modules

--- src/app/ui-controls/primeng-ui-controls/primeng-ui-controls.module.ts
+++ src/app/ui-controls/primeng-ui-controls/primeng-ui-controls.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
-import { SplitterModule  } from 'primeng/splitter';
 import {ButtonModule} from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { RatingModule } from 'primeng/rating';
 
 
 @NgModule({
   declarations: [],
   imports: [
     MenubarModule,
-    SplitterModule ,
-    ButtonModule
+    TableModule,
+    ButtonModule,
+    TagModule,
+    RatingModule,
   ],
   exports: [
     MenubarModule,
-    SplitterModule ,
-    ButtonModule
+    TableModule,
+    ButtonModule,
+    TagModule,
+    RatingModule,
   ] 
 })
 export class PrimengUiControlsModule { }
