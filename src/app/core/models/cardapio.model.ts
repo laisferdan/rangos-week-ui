886140a7e4d0cf2dd876c794a080d059d7eacb94
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-05-04 16:01:33 -0300

    feat: change properties of models

---
+++ src/app/core/models/cardapio.model.ts
@@ -0,0 +1,9 @@
+export class Cardapio {
+    public id!: number;
+    public nome_refeicao!: string;
+    public nome_alimento!: string;
+    public nome_categoria!: string;
+    public quantidade!: string;
+    public kcal!: number;
+    public dia_semana!: string;
+}
--- src/app/core/models/column.ts
+++ src/app/core/models/column.ts
@@ -1,4 +1,7 @@
+import { Cardapio } from "./cardapio.model";
+
 export interface Column {
     field: string;
     header: string;
+    details: Cardapio[];
 }
