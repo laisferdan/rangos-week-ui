f4c2d7de0f63c96befe7e819d9192b00d5f1a8ed
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-05-05 13:10:49 -0300

    feat: make Add, Edit and Delete buttons of main page show

--- src/app/ui-controls/primeng-ui-controls/primeng-ui-controls.module.ts
+++ src/app/ui-controls/primeng-ui-controls/primeng-ui-controls.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { RatingModule } from 'primeng/rating';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
-
+import { DialogModule } from 'primeng/dialog';
+import { RippleModule } from 'primeng/ripple';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
   declarations: [],
@@ -17,7 +25,17 @@ import { ToolbarModule } from 'primeng/toolbar';
     TagModule,
     RatingModule,
     ToastModule,
-    ToolbarModule
+    ToolbarModule,
+    DialogModule,
+    RippleModule,
+    ConfirmDialogModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    RadioButtonModule,
+    InputTextModule,
+    FormsModule,
+    InputNumberModule,
   ],
   exports: [
     MenubarModule,
@@ -26,7 +44,17 @@ import { ToolbarModule } from 'primeng/toolbar';
     TagModule,
     RatingModule,
     ToastModule,
-    ToolbarModule
-  ] 
+    ToolbarModule,
+    DialogModule,
+    RippleModule,
+    ConfirmDialogModule,
+    InputTextModule,
+    InputTextareaModule,
+    DropdownModule,
+    RadioButtonModule,
+    InputTextModule,
+    FormsModule,
+    InputNumberModule,
+  ],
 })
-export class PrimengUiControlsModule { }
+export class PrimengUiControlsModule {}
