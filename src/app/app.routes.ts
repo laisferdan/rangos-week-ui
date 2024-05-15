8ee4ece8f3b2f320fb46c98cad5f01888cb2b70e
estelaoli23 <estelaoliveirareato23@outlook.com>
2024-05-15 20:06:05 -0300

    feat:component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/pages/home/home.component';
+import { ArmazenamentoComponent } from './features/pages/armazenamento/armazenamento.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'armazenamento', component: ArmazenamentoComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' }
+   
 ];
---
+++ src/app/features/pages/armazenamento/armazenamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-armazenamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './armazenamento.component.html',
+  styleUrl: './armazenamento.component.css'
+})
+export class ArmazenamentoComponent {
+
+}
