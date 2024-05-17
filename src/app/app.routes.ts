6147b2568cc8c608811caf1679d4b174805a3d2e
estelaoli23 <estelaoliveirareato23@outlook.com>
2024-05-17 10:03:34 -0300

    feat:sobre-nos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/pages/home/home.component';
 import { ArmazenamentoComponent } from './features/pages/armazenamento/armazenamento.component';
+import { SobreNosComponent } from './features/pages/sobre-nos/sobre-nos.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'armazenamento', component: ArmazenamentoComponent},
+    { path: 'sobre-nos', component: SobreNosComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' }
    
 ];
