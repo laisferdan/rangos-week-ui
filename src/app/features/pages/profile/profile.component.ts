139372b8147e6c0fd22770c4268af92ffdad9f4d
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-10-19 16:52:16 -0300

    feat: add profile page

---
+++ src/app/features/pages/profile/profile.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent implements OnInit {
+  public user: any;
+  public newPassword: string = '';
+  public confirmPassword: string = '';
+  public passwordChangeSuccess: boolean = false;
+  public passwordChangeError: string = '';
+  public showChangePassword: boolean = false; 
+
+  constructor() {}
+  ngOnInit() {
+    this.user = {
+      firstName: 'Samantha',
+      name: 'Samantha de Jesus',
+      dateOfBirth: '05/04/1984',
+      email: 'mantinha23@email.com',
+    };
+  }
+
+  public changePassword() {
+    if (this.newPassword !== this.confirmPassword) {
+      this.passwordChangeError = 'Senhas não conferem';
+      return;
+    }
+
+    this.passwordChangeSuccess = true;
+    this.passwordChangeError = '';
+    this.newPassword = '';
+    this.confirmPassword = '';
+  }
+
+  public toggleChangePassword() {
+    this.showChangePassword = !this.showChangePassword;
+  }
+}
