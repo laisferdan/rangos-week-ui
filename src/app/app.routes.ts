f967035f71d5620b09b150ebdda88690cd5b1542
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-10-19 17:40:22 -0300

    feat: add storage page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/pages/home/home.component';
-import { ArmazenamentoComponent } from './features/pages/armazenamento/armazenamento.component';
 import { LoginComponent } from './features/pages/login/login.component';
 import { RegisterComponent } from './features/pages/register/register.component';
 import { ProfileComponent } from './features/pages/profile/profile.component';
 import { AboutUsComponent } from './features/pages/about-us/about-us.component';
+import { StorageComponent } from './features/pages/storage/storage.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'cadastro', component: RegisterComponent },
     { path: 'perfil', component: ProfileComponent },
-    { path: 'armazenamento', component: ArmazenamentoComponent},
+    { path: 'armazenamento', component: StorageComponent},
     { path: 'sobre-nos', component: AboutUsComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' }
    
