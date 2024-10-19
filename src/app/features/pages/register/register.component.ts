fd5f3054d3a973fdb795a19457c9237c3ca7e1f0
Laís Fernandes Daniel <laisfd@hotmail.com>
2024-10-19 16:55:49 -0300

    feat: add register page

--- src/app/features/pages/register/register.component.ts
+++ src/app/features/pages/register/register.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from '../../../shared/services/user.service';
-import User from '../../../core/models/user.model';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -11,68 +9,16 @@ import User from '../../../core/models/user.model';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent implements OnInit {
-  id: string = "";
-  cadastroForm: any;
-  
-  constructor(
-    private readonly userService: UserService,
-    private readonly formBuilder: FormBuilder,
-    private readonly route: Router,
-    private readonly activatedRoute: ActivatedRoute
-  ) {}
-  
-  onRegister() {
-    throw new Error('Method not implemented.');
-  }
+export class RegisterComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    this.id = this.activatedRoute.snapshot.params["id"];
+  onSubmit(event: Event) {
+    event.preventDefault(); // Prevent the default form submission
 
-    if (this.id) {
-      this.userService.returnUser(this.id).subscribe({
-        next: (user) => {
-          console.log(user);
+    // Implement user registration logic here
+    // For example, call a service to register the user
 
-          this.registerForm = this.formBuilder.group({
-            name: user.name,
-            email: user.email,
-            password: '',
-            permission: user.permission
-          });
-        },
-        error: (error) => console.error(error)
-      })
-    }
-
-  }
-
-  registerForm = this.formBuilder.group({
-    name: '',
-    email: '',
-    password: '',
-    permission: ''
-  });
-
-  onSubmit() {
-    const user = new User(
-      this.registerForm.value.name ?? '',
-      this.registerForm.value.email ?? '',
-      this.registerForm.value.password ?? '',
-      ''
-    );
-
-    if (this.id) {
-      user._id = this.id;
-      user.permission = this.registerForm.value.permission ?? '';
-      this.userService.updateUser(user).subscribe({
-        next: () => this.route.navigate(["/home/users"])
-      });
-    } else {
-      this.userService.addUser(user).subscribe((retorno) => {
-        console.log(retorno);
-        this.route.navigate(['/login']);
-      });
-    }
+    // After successful registration, navigate to the login page
+    this.router.navigate(['/login']);
   }
 }
